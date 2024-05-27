17522b30b72c2584dbcdfcbb199c3959c29c5c8c
noob-coder-19 <ayushabhishekkumar19@gmail.com>
2024-05-27 21:37:00 +0530

    Added basic stories for iconbutton

---
+++ apps/docs/stories/iconbutton.stories.tsx
@@ -0,0 +1,52 @@
+import { IconButton } from '@noobcoder19/design-system';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof IconButton> = {
+  component: IconButton,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof IconButton>;
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/react/api/csf
+ * to learn how to use render functions.
+ */
+export const Inaccessible: Story = {
+  args: {
+    children: (
+      <svg
+        height="1em"
+        viewBox="0 0 24 24"
+        width="1em"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95"
+          fill="currentColor"
+        />
+      </svg>
+    ),
+  },
+};
+
+export const Accessible: Story = {
+  args: {
+    'aria-label': 'facebook-icon',
+    children: (
+      <svg
+        height="1em"
+        viewBox="0 0 24 24"
+        width="1em"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95"
+          fill="currentColor"
+        />
+      </svg>
+    ),
+  },
+};
