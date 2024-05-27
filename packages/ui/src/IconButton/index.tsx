996dba1f0e8ad9c4389919fa134f59d8cd64dbfa
noob-coder-19 <ayushabhishekkumar19@gmail.com>
2024-05-27 16:32:00 +0530

    Added initial styles for icon button

---
+++ packages/ui/src/IconButton/index.tsx
@@ -0,0 +1,44 @@
+import { styled } from 'styled-components';
+import tokens from '@noobcoder19/foundation';
+import { forwardRef } from 'react';
+import type IconButtonProps from './types';
+
+const IconButtonSyled = styled.button<IconButtonProps>`
+  all: unset;
+  cursor: pointer;
+  padding: ${tokens.spacings[4].value} ${tokens.spacings[4].value};
+
+  border-radius: ${(props) => tokens.radius[props.radius || 'infinity'].value};
+  color: ${tokens.colors.neutral.white.value};
+  background-color: ${(props) =>
+    tokens.colors[props.color || 'primary'][500].value};
+`;
+
+export const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
+  (
+    {
+      disabled = false,
+      loading = false,
+      color = 'primary',
+      radius = 'infinity',
+      ...props
+    },
+    ref,
+  ) => {
+    return (
+      <IconButtonSyled
+        color={color}
+        disabled={disabled || loading}
+        radius={radius}
+        ref={ref}
+        {...props}
+      >
+        {props.children}
+      </IconButtonSyled>
+    );
+  },
+);
+
+IconButton.displayName = 'IconButton';
+
+export default IconButton;
