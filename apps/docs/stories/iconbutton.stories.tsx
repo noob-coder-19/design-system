38137e194114f7c4c6a8f2b76262157fec24a818
noob-coder-19 <ayushabhishekkumar19@gmail.com>
2024-05-31 08:21:00 +0530

    Refactor IconButton stories to include common args

--- apps/docs/stories/iconbutton.stories.tsx
+++ apps/docs/stories/iconbutton.stories.tsx
@@ -3,6 +3,21 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof IconButton> = {
   component: IconButton,
+  args: {
+    children: (
+      <svg
+        height="1em"
+        viewBox="0 0 24 24"
+        width="1em"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95"
+          fill="currentColor"
+        />
+      </svg>
+    ),
+  },
   argTypes: {
     type: {
       control: { type: 'radio' },
@@ -25,58 +40,18 @@ type Story = StoryObj<typeof IconButton>;
  * to learn how to use render functions.
  */
 export const Inaccessible: Story = {
-  args: {
-    children: (
-      <svg
-        height="1em"
-        viewBox="0 0 24 24"
-        width="1em"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95"
-          fill="currentColor"
-        />
-      </svg>
-    ),
-  },
+  args: {},
 };
 
 export const Accessible: Story = {
   args: {
     'aria-label': 'facebook-icon',
-    children: (
-      <svg
-        height="1em"
-        viewBox="0 0 24 24"
-        width="1em"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95"
-          fill="currentColor"
-        />
-      </svg>
-    ),
   },
 };
 
 export const CustomColor: Story = {
   args: {
     'aria-label': 'facebook-icon',
-    children: (
-      <svg
-        height="1em"
-        viewBox="0 0 24 24"
-        width="1em"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95"
-          fill="currentColor"
-        />
-      </svg>
-    ),
     color: 'success',
   },
 };
@@ -84,19 +59,6 @@ export const CustomColor: Story = {
 export const Disabled: Story = {
   args: {
     'aria-label': 'facebook-icon',
-    children: (
-      <svg
-        height="1em"
-        viewBox="0 0 24 24"
-        width="1em"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95"
-          fill="currentColor"
-        />
-      </svg>
-    ),
     color: 'success',
     disabled: true,
   },
@@ -105,19 +67,6 @@ export const Disabled: Story = {
 export const Loading: Story = {
   args: {
     'aria-label': 'facebook-icon',
-    children: (
-      <svg
-        height="1em"
-        viewBox="0 0 24 24"
-        width="1em"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95"
-          fill="currentColor"
-        />
-      </svg>
-    ),
     color: 'success',
     loading: true,
   },
