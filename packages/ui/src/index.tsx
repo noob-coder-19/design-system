376464d86cd2dd1d93a5288ab83421a3c025c079
noob-coder-19 <ayushabhishekkumar19@gmail.com>
2024-06-02 09:33:00 +0530

    Added exports for button component

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,2 +1,5 @@
 export { default as IconButton } from './IconButton/index';
 export type { default as IconButtonProps } from './IconButton/types';
+
+export { default as Button } from './Button/index';
+export type { default as ButtonProps } from './Button/types';
