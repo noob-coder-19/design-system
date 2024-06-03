5b4ee9bf4b3dcda48e400432794db79dc89a4433
noob-coder-19 <ayushabhishekkumar19@gmail.com>
2024-06-03 07:34:00 +0530

    Added basic stories for button component

---
+++ apps/docs/stories/button.stories.tsx
@@ -0,0 +1,21 @@
+import { Button } from '@noobcoder19/design-system';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/react/api/csf
+ * to learn how to use render functions.
+ */
+export const Default: Story = {
+  args: {
+    children: 'Click me',
+  },
+};
