e57e1a301f248b52733b08089f4bb4f3fd7de90b
noob-coder-19 <ayushabhishekkumar19@gmail.com>
2024-06-04 09:43:00 +0530

    Refactor Button component styles for disabled state

--- packages/ui/src/Button/index.tsx
+++ packages/ui/src/Button/index.tsx
@@ -8,6 +8,11 @@ const ButtonSyled = styled.button<ButtonProps>`
   cursor: pointer;
   padding: ${tokens.spacings[8].value} ${tokens.spacings[16].value};
 
+  &:disabled {
+    cursor: not-allowed;
+    opacity: 40%;
+  }
+
   border-radius: ${(props) => tokens.radius[props.radius || 'small'].value};
   color: ${tokens.colors.neutral.white.value};
   background-color: ${(props) =>
