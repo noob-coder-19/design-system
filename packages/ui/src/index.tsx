7e86ed9789556a4eda16ad7e4819f2155926816d
noob-coder-19 <ayushabhishekkumar19@gmail.com>
2024-06-11 08:42:00 +0530

    Added exports for Checkbox component and its types

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -6,3 +6,6 @@ export type { default as ButtonProps } from './Button/types';
 
 export { default as Switch } from './Switch/index';
 export type { default as SwitchProps } from './Switch/types';
+
+export { default as Checkbox } from './Checkbox/index';
+export type { default as CheckboxProps } from './Checkbox/types';
