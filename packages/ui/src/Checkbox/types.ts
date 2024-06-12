07106ee9325f04122305350caea5a490383eaede
noob-coder-19 <ayushabhishekkumar19@gmail.com>
2024-06-12 09:12:00 +0530

    Updated types for checkbox

--- packages/ui/src/Checkbox/types.ts
+++ packages/ui/src/Checkbox/types.ts
@@ -3,11 +3,12 @@ import type tokens from '@noobcoder19/foundation';
 export interface DivSyledProps {
   color?: keyof typeof tokens.colors;
   disabled?: boolean;
+  size?: string;
 }
 
 export default interface CheckboxProps
   extends Omit<React.HTMLAttributes<HTMLInputElement>, 'color'>,
     DivSyledProps {
   checked?: boolean;
-  size?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
