2c10bb77fe4572db09d877af7f2192d8ee1ce3f8
noob-coder-19 <ayushabhishekkumar19@gmail.com>
2024-06-12 07:12:00 +0530

    Refactor CheckedIcon component to use token-based dimensions

--- packages/ui/src/common/Icons/checked-icon.tsx
+++ packages/ui/src/common/Icons/checked-icon.tsx
@@ -1,10 +1,19 @@
 import type { SVGProps } from 'react';
+import tokens from '@noobcoder19/foundation';
 
-export default function CheckedIcon(
-  props: SVGProps<SVGSVGElement>,
-): JSX.Element {
+export default function CheckedIcon({
+  width = tokens.spacings[20].value,
+  height = tokens.spacings[20].value,
+  ...props
+}: SVGProps<SVGSVGElement>): JSX.Element {
   return (
-    <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <svg
+      height={height}
+      viewBox="0 0 16 16"
+      width={width}
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
       <path
         d="M4.5 2A2.5 2.5 0 0 0 2 4.5v7A2.5 2.5 0 0 0 4.5 14h7a2.5 2.5 0 0 0 2.5-2.5v-7A2.5 2.5 0 0 0 11.5 2zm6.354 4.854l-3.5 3.5a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 9.293l3.146-3.147a.5.5 0 0 1 .708.708"
         fill="currentColor"
