e71059834ca46dcae4b8ccb58a0c562e20a765e7
noob-coder-19 <ayushabhishekkumar19@gmail.com>
2024-06-15 16:34:00 +0530

    Refactor types for radio

--- packages/ui/src/Radio/types.ts
+++ packages/ui/src/Radio/types.ts
@@ -4,10 +4,9 @@ export interface DivStyledProps {
   color?: keyof typeof tokens.colors;
   disabled?: boolean;
   checked?: boolean;
+  size?: string;
 }
 
 export default interface RadioProps
   extends Omit<React.HTMLAttributes<HTMLInputElement>, 'color'>,
-    DivStyledProps {
-  size?: string;
-}
+    DivStyledProps {}
